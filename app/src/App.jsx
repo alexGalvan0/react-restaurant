e5e1b75b4e61f3c8cbf726d437bd764273015ff0
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-12 22:57:47 +0000

    added cards

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Card from "./componenet/Card";
 import Button from "./componenet/Button";
 import Spinner from "./componenet/Spinner";
+import Header from "./componenet/Header"
 
 export default function App(){
 
@@ -28,14 +29,18 @@ export default function App(){
     if (data.length === 0) return <Spinner />
 
     return(
-        <div className="App container-fluid ">
-          <div className="row">
-            <div className="col"style={{height:"100vh"}}>
+        <div className="App container-fluid text-center">
+          <Header />
+          
+            <div className="col text-center"style={{height:"100vh"}}>
             <Button text='Breakfast' handleClick={setType} />
             <Button text='Lunch' handleClick={setType}/>
             <Button text='Dinner' handleClick={setType}/>
             <Button text='Dessert' handleClick={setType}/>
-            {filteredMenu.map((d) => <Card key={d.id} title={d.title} category={d.category.title} description={d.description} price={'$'+d.price} />)}
+            <div className="d-flex justify-content-center">
+            <div className="row">
+              {filteredMenu.map((d) => <Card key={d.id} title={d.title} category={d.category.title} description={d.description} price={'$'+d.price} />)}
+            </div>
             </div>
           </div>
         </div>
--- app/src/componenet/Card.jsx
+++ app/src/componenet/Card.jsx
@@ -1,7 +1,6 @@
 function Card({ title, description,price,category }){
     return(
         <div className="card" style={{width:"18rem"}}>
-
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
