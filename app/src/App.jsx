efb9c56406dcd90be91db35347efb44a8421d0e4
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-12 19:42:40 +0000

    query

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
+import './App.css'
+
+
 import Card from "./componenet/Card";
+import Nav from "./componenet/Nav";
+import Spinner from "./componenet/Spinner";
 
 export default function App(){
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [page, setPage] = useState('Dinner');
 
     const url = 'https://astute-baton-362318.ue.r.appspot.com/api/json/'
 
@@ -12,15 +18,30 @@ export default function App(){
         axios.get(url)
           .then((resp) => {
             setData(resp.data);
-            console.log(resp)
           })
       }, []);
 
-    if (data.length === 0) return null
+      const dinMenu = data.filter((item) =>{
+        return item.category.title == page
+      })
 
-    return(
-        <div className="App">
-                {data.map((d) => <Card key={d.title} title={d.title} description={d.description} price={'$'+d.price} />)}
+    if (data.length === 0) return (
+      <div className="App container-fluid border">
+      <div className="row">
+        <div className="col d-flex justify-content-center align-items-center "style={{height:"100vh"}}>
+          <Spinner />
+        </div>
+      </div>
+    </div>
+    )
+
+    if(page == 'Dinner') return(
+        <div className="App container-fluid ">
+          <div className="row">
+            <div className="col"style={{height:"100vh"}}>
+            {dinMenu.map((d) => <Card key={d.title} title={d.title} category={d.category.title} description={d.description} price={'$'+d.price} />)}
+            </div>
+          </div>
         </div>
     )
 }
