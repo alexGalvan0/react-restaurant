4c61d368a547571329c9e27760b6495b04fe13b4
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-13 19:17:04 +0000

    mobile

--- app/src/componenet/Button.jsx
+++ app/src/componenet/Button.jsx
@@ -1,13 +1,13 @@
 
 
-export default function Button({text, handleClick,image}){
+export default function Button({text, handleClick,image,className}){
 const click = () => {
   handleClick(text)
 }
     return(
 
-        <div className="gap-2">
-          <img className="pe-auto"aria-disabled="true" src={image} onClick={click} width="500" style={{height:"15rem",width:"20rem"}}/>
+        <div className="col-xl-2">
+          <img className="img-fluid ratio-2x2"aria-disabled="true" src={image} onClick={click}/>
           <h3>{text}</h3>
         </div>
 
