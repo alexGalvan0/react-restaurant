f632cfc40f2b6311cac5ca5e5f8b3c9a31da0ddd
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-14 13:00:10 +0000

    cleaned up code

---
+++ app/src/componenet/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return(
+    <div className="p-4 p-md-5 mb-4 rounded text-bg-dark">
+        <div className="col-md-6 px-0">
+        <h1 className="display-4 fst-italic">We have a Friendly Menu!</h1>
+        <p className="lead my-3">Search our Menu!</p>
+    </div>
+  </div>
+    )
+}
+
+export default Header;
--- app/src/componenet/Selector.jsx
+++
@@ -1,9 +0,0 @@
-export default function Selector({image,text}){
-    return(
-        <div className="col">
-            <img src={image} className="bd-placeholder-img rounded-circle" width="500" height="500"/>
-
-            <h2 className="fw-normal">{text}</h2>
-      </div>
-    )
-}
